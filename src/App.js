1e4a7b4cfdd903f6ee8cea63e77981e34cdd5f94
dmaxwell7 <desroymaxwell@gmail.com>
2023-01-23 15:00:29 -0600

    update modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -13,31 +13,57 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 
 
-const App = () => {
+// const App = () => {
 
-  const { isLoading } = useAuth0();
+//   const { isLoading } = useAuth0();
+//   if (isLoading) return <div>Loading...</div>
+
+//   return (
+//   <>
+//     <LoginButton />
+//     <LogoutButton />
+//     <Profile />
+//     <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/exercise/:id" element={<ExerciseDetail />} />
+//       </Routes>
+//       <Footer />
+//     </Box>
+//   </>
+//   )
+// }
+
+// export default App
+
+
+const App = () => {
+  const { isLoading, isAuthenticated } = useAuth0();
   if (isLoading) return <div>Loading...</div>
 
   return (
-  <>
-    <LoginButton />
-    <LogoutButton />
-    <Profile />
-    <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetail />} />
-      </Routes>
-      <Footer />
-    </Box>
-  </>
+    <>
+      {!isAuthenticated && <LoginButton />}
+      {isAuthenticated && 
+      <>
+        <LogoutButton />
+        <Profile />
+        <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
+          </Routes>
+          <Footer />
+        </Box>
+      </>
+      }
+    </>
   )
 }
 
 export default App
-
-
 
 
 
